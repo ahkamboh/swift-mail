a846b556d694935069bf156e0f9f85d0ecaec223
kambohdiv <kambohdiv@gmail.com>
2024-07-29 05:00:03 +0500

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SessionWrapper from "./components/SessionWrapper";
+import Head from 'next/head'; // Import Head component
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -47,6 +48,19 @@ export default function RootLayout({
 }: RootLayoutProps) {
   return (
     <html lang="en">
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6MRRSFSM4Z"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6MRRSFSM4Z');
+            `,
+          }}
+        />
+      </Head>
       <SessionWrapper>
         <body className={inter.className}>{children}</body>
       </SessionWrapper>
